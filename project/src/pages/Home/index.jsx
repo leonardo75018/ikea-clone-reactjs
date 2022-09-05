ef72ae1e7c297925194564a9591f0f0da385d464
leonardo kabongo <leonardinho@MBP-de-leonardo.lan>
2022-09-05 10:33:20 +0200

    start

---
+++ project/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+
+const Home = () =>{
+    return(
+        <div>
+            <h2>Page home</h2>
+        </div>
+    )
+}
+
+export default Home;
